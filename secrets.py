06817dd58e34ea0fb767b1bf5759051c215b6fd0
CTDave <88465578+CTDave001@users.noreply.github.com>
2023-04-25 19:28:37 -0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -2,12 +2,9 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), 'new.env')
+dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-#DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 DISCORD_SECRET = os.environ.get("DISCORD_SECRET")
 GUILD_ID = os.environ.get("GUILD_ID")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-the_id_of_your_guild = 1100154123116101673
-
